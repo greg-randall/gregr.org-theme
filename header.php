e7cee18ad88731af84638ba0e4d150460fe5e916
Greg Randall <gregrr@gmail.com>
2019-09-10 14:55:01 -0500

    cleaned things and fixed some bugs

--- header.php
+++ header.php
@@ -1,20 +1,18 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title><?php wp_title(''); ?></title>
 
     <?php wp_head(); ?>
   </head>
-  <body>
-  <div class="bg-light">
-
-                <h1 class="pl-3 pr-3 pt-3 pb-4 text-center">Greg.Randall</h1>
-
+  <body style="background-image: url('wp-content/themes/gregr-org_theme/background.gif');background-repeat: repeat;">
+                <div class="jumbotron jumbotron-fluid" style="padding-top:25px;padding-bottom:25px;background-color: rgba(204, 204, 204, 0.3);">
+        <div class="container text-center">
+            <h1 class="display-3">Greg.Randall</h1>
         </div>
+    </div>
