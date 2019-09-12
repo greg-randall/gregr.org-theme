c6d75e1b415a4af7f29fbee0b1ff3fb155c4310b
Greg Randall <gregrr@gmail.com>
2019-09-12 15:23:14 -0500

    added pagination, might need some tweaking

--- index.php
+++ index.php
@@ -1,20 +1,45 @@
 <?php get_header(); ?>
 
 <?php
-    $postslist = get_posts( array( 'posts_per_page' => 5 ) );
+    $posts_per_page = 5;
+    if (array_key_exists('p', $_GET)) {
+        $page = intval(trim($_GET['p']));
+    } else {
+        $page = 1;
+    }
+    $postslist = get_posts(array(
+        'posts_per_page' => $posts_per_page,
+        'paged' => $page
+    ));
     echo '<div class="container">';
-    foreach ( $postslist as $post ) :?> 
+    foreach ($postslist as $post):
+?> 
             <h2 style="margin-left:-10px;"><?php echo $post->post_title; ?></h2>
-            <p style="margin-left:10px;"><em><a href="<?php echo esc_url( get_permalink($post->id)); ?>"><?php echo date('F jS Y',strtotime($post->post_date)); ?></a></em></p> 
-            <?php echo apply_filters( 'the_content', $post->post_content ) ?>
-            <hr>
+            <p style="margin-left:10px;"><em><a href="<?php echo esc_url(get_permalink($post->id)); ?>"><?php echo date('F jS Y', strtotime($post->post_date)); ?></a></em></p> 
+            <?php
+        echo apply_filters('the_content', $post->post_content);
+?>
+           <hr>
     <?php
-    endforeach; 
+    endforeach;
+    $count_posts     = wp_count_posts();
+    $published_posts = $count_posts->publish;
+    echo '<div class="text-center">';
+    if ($page > 1) {
+        $page_calc= $page - 1;
+        echo "<a href=\"//gregr.org/index.php?p=$page_calc\">Previous Page</a>";
+    }
+    if ($posts_per_page * $page < $published_posts) {
+        $page_calc= $page + 1;
+        echo "<a href=\"//gregr.org/index.php?p=$page_calc\">Next Page</a>";
+    }
     echo '</div>';
-
+    echo '</div>';
     wp_reset_postdata();
 ?>
 
 
 
-<?php get_footer(); ?>
+<?php
+    get_footer();
+?>
