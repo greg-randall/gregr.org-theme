0237134a1f3b4ff1ba54ae1490fb796075c9f2b1
Greg Randall <gregrr@gmail.com>
2019-09-12 15:21:02 -0500

    added an actual footer, including copyright thing

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+<footer class="page-footer font-small" style="background-color: #999;">
+	
+	<div class="text-center">
+		<a style="color:black;" href="//gregr.org"><?php echo get_bloginfo('name'); ?></a> <?php echo date("Y"); ?>©
+	</div>
+</footer>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
