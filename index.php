e82051327d0c4edd03fd79263424645d3b8ae5d4
Greg Randall <gregrr@gmail.com>
2019-09-13 09:19:43 -0500

    fixed some pagination issues. so if you enter a non-real pagenumber it redirects you to the last page --ie if you have pg 200 and there are only 5pgs on the site itll put you at pg 5--, if you enter some words or something nonsense itll go to the front page

--- index.php
+++ index.php
@@ -2,11 +2,32 @@
 
 <?php
     $posts_per_page = 5;
-    if (array_key_exists('p', $_GET)) {
-        $page = intval(trim($_GET['p']));
+
+    $count_posts     = wp_count_posts();
+    $published_posts = $count_posts->publish;
+    
+    if($published_posts/$posts_per_page==round($published_posts/$posts_per_page)){
+        $last_page = $published_posts/$posts_per_page;
+    }else{
+        $last_page = round($published_posts/$posts_per_page) + 1;
+    }
+
+
+    if ( array_key_exists('p', $_GET) & is_numeric($_GET['p']) ) {
+        
+        echo "<!-- debug 1 url-". $_GET['p'] ." published-posts- $published_posts math- $last_page-->";
+        if($_GET['p']<=$last_page){
+            $page = intval(trim($_GET['p']));
+            echo "<!-- debug 2a $page -->";
+        }else {
+            $page = $last_page;
+            echo "<!-- debug 2b $page -->";
+        }
+
     } else {
         $page = 1;
     }
+
     $postslist = get_posts(array(
         'posts_per_page' => $posts_per_page,
         'paged' => $page
@@ -24,17 +45,21 @@
            <hr>
     <?php
     endforeach;
-    $count_posts     = wp_count_posts();
-    $published_posts = $count_posts->publish;
+
     echo '<div class="text-center">';
     if ($page > 1) {
-        $page_calc= $page - 1;
-        echo "<a href=\"//gregr.org/index.php?p=$page_calc\">Previous Page</a> ";
+        $page_calc = $page - 1;
+        if($page_calc==1){
+            echo "<a href=\"//gregr.org/\">Previous Page</a> ";
+        }else{
+            echo "<a href=\"//gregr.org/index.php?p=$page_calc\">Previous Page</a> "; 
+        }
     }
     if ($posts_per_page * $page < $published_posts) {
         $page_calc= $page + 1;
         echo " <a href=\"//gregr.org/index.php?p=$page_calc\">Next Page</a>";
     }
+    echo "<br>$page/$last_page";
     echo '</div>';
     echo '</div>';
     wp_reset_postdata();
